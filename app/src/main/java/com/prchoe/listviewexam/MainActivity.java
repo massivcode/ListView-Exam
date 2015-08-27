806d9d1f700656834d24293c9fbe3b487442d432
prChoe <massivcode@gmail.com>
2015-08-27 15:04:29 +0900

    String 한 개만 출력하는 ListView

--- app/src/main/java/com/prchoe/listviewexam/MainActivity.java
+++ app/src/main/java/com/prchoe/listviewexam/MainActivity.java
@@ -1,37 +1,61 @@
+
 package com.prchoe.listviewexam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> mData;
+    private ArrayAdapter<String> mAdapter;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 1. 데이터 준비
+        initData();
+
+        // 2. Adapter 준비
+        initAdapter();
+
+        // 3. ListView에 Adapter를 장착
+        initListView();
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    // Data 준비
+    private void initData() {
+        mData = new ArrayList();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        for (int i = 1; i <= 100; i++) {
+            mData.add("Item " + i);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    // Adapter 준비
+    private void initAdapter() {
+
+        // Parameter : Context , 아이템 하나에 대한 레이아웃 , 데이터
+        // simple_list_item_1 : 텍스트뷰 하나만 있는 레이아웃
+        // getApplicationContext(), Activity.this, this 중에 getApplicationContext()를 권장
+        // memory leak를 피하기 위해 권장함. 익명 클래스나 내부 클래스에서는 this가 안됩니당.
+        mAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, mData);
+
+    }
+
+    // ListView에 adapter 설정
+    private void initListView() {
+        mListView = (ListView)findViewById(R.id.list_view);
+        mListView.setAdapter(mAdapter);
+    }
+
 }
