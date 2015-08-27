b55bb7d29cae2d770483ad348b1d4ac1f933351b
prChoe <massivcode@gmail.com>
2015-08-27 16:10:18 +0900

    String 두 개를 출력하는 ListView - SimpleAdapter

--- app/src/main/java/com/prchoe/listviewexam/MainActivity.java
+++ app/src/main/java/com/prchoe/listviewexam/MainActivity.java
@@ -3,16 +3,18 @@ package com.prchoe.listviewexam;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<String> mData;
-    private ArrayAdapter<String> mAdapter;
+    private List<Map<String, String>> mData;
+    private SimpleAdapter mAdapter;
     private ListView mListView;
 
     @Override
@@ -37,7 +39,10 @@ public class MainActivity extends AppCompatActivity {
         mData = new ArrayList();
 
         for (int i = 1; i <= 100; i++) {
-            mData.add("Item " + i);
+            Map<String, String> data = new HashMap<>();
+            data.put("item", "Item " + i);
+            data.put("subitem", "Sub Item " + i);
+            mData.add(data);
         }
     }
 
@@ -48,7 +53,12 @@ public class MainActivity extends AppCompatActivity {
         // simple_list_item_1 : 텍스트뷰 하나만 있는 레이아웃
         // getApplicationContext(), Activity.this, this 중에 getApplicationContext()를 권장
         // memory leak를 피하기 위해 권장함. 익명 클래스나 내부 클래스에서는 this가 안됩니당.
-        mAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, mData);
+//        mAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, mData);
+
+        // Parameter : Context, 데이터 객체, 레이아웃, 데이터를 get할 key의 배열들, 데이터를 표시할 레이아웃의 텍스트 1,2
+        mAdapter = new SimpleAdapter(getApplicationContext(), mData, android.R.layout.simple_list_item_2,
+                new String[] {"item", "subitem"},
+                new int[] {android.R.id.text1, android.R.id.text2});
 
     }
 
