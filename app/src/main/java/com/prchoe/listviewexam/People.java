c4fcb80ba013c44bfbd5779e4173100fdf9f9449
prChoe <massivcode@gmail.com>
2015-08-31 10:34:35 +0900

    Custom 어댑터 작성 1. Data 클래스 작성

---
+++ app/src/main/java/com/prchoe/listviewexam/People.java
@@ -0,0 +1,44 @@
+package com.prchoe.listviewexam;
+
+/**
+ * Created by massivCode on 2015-08-31.
+ */
+public class People {
+
+    private String name;
+    private String phoneNumber;
+    private int imageResourceId;
+
+    public People() {
+    }
+
+    public People(String name, String phoneNumber, int imageResourceId) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public void setImageResourceId(int imageResourceId) {
+        this.imageResourceId = imageResourceId;
+    }
+}
